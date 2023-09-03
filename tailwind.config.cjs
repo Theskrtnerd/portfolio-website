a7907e53e5bee06c683c168867468ea1ce3e1d28
Theskrtnerd <tvbbd2@gmail.com>
2023-09-03 23:42:21 +0930

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        'sans': ['Poppins', 'sans-serif' ,defaultTheme.fontFamily.sans],
+      },
+			backgroundImage: {
+				'home': "url('/src/images/homeBackground.svg')"
+			},
+			colors: {
+				'text': '#f8fafc',
+				'background': '#131526',
+				'primary': '#74f8ff',
+				'secondary': '#ebf1ff',
+				'accent': '#0e82f6',
+			}
+		},
 	},
 	plugins: [],
 }
