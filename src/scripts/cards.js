3f057ff6889783d3a11a095329912eb0c68c1b80
Theskrtnerd <tvbbd2@gmail.com>
2023-09-21 03:59:49 +0930

    progress

---
+++ src/scripts/cards.js
@@ -0,0 +1,16 @@
+function card(){
+  document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
+}
+
+card();
+
+document.addEventListener('astro:after-swap', card);
---
+++ src/scripts/scrabble.js
@@ -0,0 +1,36 @@
+function scrabble(){
+  let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  letters = letters.toLowerCase();
+
+  let interval = null;
+
+  document.querySelector("#scrabble").onmouseover = event => {  
+    let iteration = 0;
+    
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if(index < iteration) {
+            return event.target.dataset.value[index];
+          }
+        
+          return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+      
+      if(iteration >= event.target.dataset.value.length){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 30);
+  }
+}
+
+scrabble();
+
+
+document.addEventListener('astro:after-swap', scrabble);
